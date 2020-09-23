11c0bd2721c650afc4c70cfdcc1659315e26d996
Tiago Dela Rosa <t.dela.rosa.lopes@accenture.com>
2020-09-22 23:11:04 -0300

    brief text

--- pages/dashboard/home.vue
+++ pages/dashboard/home.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Dashboard - Home</h1>
+  <div>
+    <h1>Dashboard - Home</h1>
+    <p>
+      Dashboard coming soon, feel free to configure your data, look up at the
+      sidebar menus
+    </p>
+  </div>
 </template>
 <script>
 export default {
