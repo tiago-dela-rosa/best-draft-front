f379d9e5bf07c074377f3d686ee665a5dfe61843
Tiago Dela Rosa <t.dela.rosa.lopes@accenture.com>
2020-09-25 20:30:34 -0300

    layout home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,34 +9,5 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Login',
-          to: '/login'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+export default {}
 </script>
---
+++ layouts/website.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app id="home" dark>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="css">
+#home {
+  background-image: url('../static/images/background_home.jpg');
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
